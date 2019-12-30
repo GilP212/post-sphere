809f401c94fb68eb41b951977ed3cb17b59b0fbd
Gil Palas <gil5809@gmail.com>
2019-12-30 13:29:05 +0200

    Added MongoDB and Mongoose functions.

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,21 @@
 const express    = require("express"),
       app        = express(),
-      bodyParser = require("body-parser");
+      bodyParser = require("body-parser"),
+      mongoose   = require("mongoose");
+
+const Post = require('./models/post');
+
+//DB SETUP
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useUnifiedTopology', true);
+
+mongoose.connect("mongodb+srv://dagil123:gilp7466@cluster0-ox8cv.mongodb.net/post-sphere?retryWrites=true&w=majority")
+.then(() => {
+  console.log('Connected to DB!');
+})
+.catch(() => {
+  console.log('Connection to DB failed!');
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -20,30 +35,40 @@ next();
 });
 
 app.post("/api/posts", (req, res, next) => {
-  const post = req.body;
-  console.log();
-  res.status(201).json({
-    message: "Post Added Succesfully"
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content
   });
+
+  post.save()
+    .then(createdPost => {
+      res.status(201).json({
+        message: "Post Added Succesfully.",
+        postId: createdPost._id
+      });
+    });
+
 });
 
 app.get("/api/posts", (req, res, next) => {
-  const posts = [
-    {
-      id: "1234",
-      title: "First Post",
-      content: "First Content."
-    },
-    {
-      id: "2234",
-      title: "Second Post",
-      content: "Second Content."
-    }
-  ];
-  res.status(200).json({
-    message: "Posts fetched successfully!",
-    posts: posts
-  });
+  Post.find()
+    .then(documents => {
+      res.status(200).json({
+        message: "Posts fetched successfully!",
+        posts: documents
+      });
+    });
+});
+
+app.delete("/api/posts/:id", (req, res, next) => {
+  Post
+    .deleteOne({_id: req.params.id})
+    .then(result => {
+      console.log(result);
+      res.status(200).json({
+        message: "Post deleted!"
+      });
+    });
 });
 
 module.exports = app;
