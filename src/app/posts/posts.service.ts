a308cd4d761349d054d9f1a9d870830d29defd7a
Gil Palas <gil5809@gmail.com>
2020-01-01 19:36:55 +0200

    Saving post redirects back to '/'

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 import { Subject } from 'rxjs'; //  Observables package
 import { map } from 'rxjs/operators';
 
@@ -10,7 +11,10 @@ export class PostsService {
   private posts: Post[] = [];
   private postsUpdated = new Subject<Post[]>();
 
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    private router: Router
+    ) {}
 
   getPosts() {
     this.http
@@ -49,6 +53,7 @@ export class PostsService {
         post.id = id;
         this.posts.push(post);
         this.postsUpdated.next([...this.posts]);
+        this.router.navigate(['/']);
       });
   }
 
@@ -62,6 +67,7 @@ export class PostsService {
         updatedPosts[oldPostIndex] = post;
         this.posts = updatedPosts;
         this.postsUpdated.next([...this.posts]);
+        this.router.navigate(['/']);
       });
   }
 
