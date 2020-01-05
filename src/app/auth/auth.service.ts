ca9f1d249404c11bcd44041722643b9c4c3e6b0a
Gil Palas <gil5809@gmail.com>
2020-01-05 22:21:53 +0200

    Authentication uses token now.

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -5,7 +5,13 @@ import { AuthData } from './auth-data.model';
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
+  private token: string;
+
   constructor(private http: HttpClient) {}
+
+  getToken() {
+    return this.token;
+  }
 
   createUser(email: string, password: string) {
     const authData: AuthData = {email, password};
@@ -20,9 +26,10 @@ export class AuthService {
     const authData: AuthData = {email, password};
 
     this.http
-      .post('http://localhost:3000/api/users/login', authData)
+      .post<{token: string}>('http://localhost:3000/api/users/login', authData)
       .subscribe(response => {
-        console.log(response);
+        const token = response.token;
+        this.token = token;
       });
   }
 }
