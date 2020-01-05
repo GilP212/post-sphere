7b063fca882a2a65d539adba4ff0c2d9002d16d6
Gil Palas <gil5809@gmail.com>
2020-01-05 20:48:36 +0200

    Login now creates and sends token to frontend .

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,5 +1,6 @@
 const express = require("express"),
-      bcrypt = require("bcrypt");
+      bcrypt = require("bcrypt"),
+      jwt = require("jsonwebtoken");
 
 const User = require("../models/user");
 
@@ -26,7 +27,43 @@ router.post("/signup", (req, res, next) => {
         error: err
       });
     });
+});
 
+router.post("/login", (req, res, next) => {
+  let fetchedUser;
+
+  User
+    .findOne({ email: req.body.email})
+    .then(user => {
+      if (!user) {
+        return res.status(401).json({
+          message: "Authentication failed!"
+        });
+      }
+      fetchedUser = user;
+      return bcrypt.compare(req.body.password, user.password);
+    })
+    .then(result => {
+      if (!result) {
+        return res.status(401).json({
+          message: "Authentication failed!"
+        });
+      }
+      //  Authentication Successful
+      const token = jwt.sign(
+        { email: fetchedUser.email, userId: fetchedUser._id },
+        'secret_this_should_be_longer',
+        { expiresIn: '1h' }
+      );
+      res.status(200).json({
+        token: token
+      });
+    })
+    .catch(err => {
+      return res.status(401).json({
+        message: "Authentication failed!"
+      });
+    });
 });
 
 module.exports = router;
