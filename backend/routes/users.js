1360b960152c8337ae99a021317beb6a9f4739e1
Gil Palas <gil5809@gmail.com>
2020-01-05 17:59:37 +0200

    Added backend signup POST logic.

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,31 @@
-const express = require("express");
+const express = require("express"),
+      bcrypt = require("bcrypt");
+
+const User = require("../models/user");
 
 const router = express.Router();
 
 router.post("/signup", (req, res, next) => {
-
+  bcrypt
+    .hash(req.body.password, 10)
+    .then(hash => {
+      const user = new User({
+        email: req.body.email,
+        password: hash
+      });
+      return user.save();
+    })
+    .then(result => {
+      res.status(201).json({
+        message: 'User created!',
+        result: result
+      });
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: err
+      });
+    });
 });
 
 module.exports = router;
