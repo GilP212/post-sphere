066e02348a0ccce4fa27d6ea721bbe58cdb80059
Gil Palas <gil5809@gmail.com>
2020-01-13 20:35:56 +0200

    Seperated Auth-Module.

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,22 @@
+// EXTERNAL IMPORTS
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+// LOCAL IMPORTS
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
+import { AngularMaterialModule } from '../angular-material.module';
+
+@NgModule({
+  declarations: [
+    LoginComponent,
+    SignupComponent
+  ],
+  imports: [
+    CommonModule,
+    AngularMaterialModule,
+    FormsModule
+  ]
+})
+export class AuthModule {}
